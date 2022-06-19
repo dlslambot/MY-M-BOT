82f4168a81bd0a1d2581769d024efa4fc20ee68c
dlslambot <106543696+dlslambot@users.noreply.github.com>
2022-06-19 19:46:56 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -33,11 +33,6 @@ class MirrorStatus:
     STATUS_CHECKING = "CheckingUp...📝"
     STATUS_SEEDING = "Seeding...🌧"
 
-
-
-PROGRESS_MAX_SIZE = 100 // 8
-PROGRESS_INCOMPLETE = ['○', '◔', '◑', '◕', '⬤']
-
 SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
 
 
@@ -110,15 +105,11 @@ def get_progress_bar_string(status):
     total = status.size_raw() / 8
     p = 0 if total == 0 else round(completed * 100 / total)
     p = min(max(p, 0), 100)
-    cFull = p // 8
-    cPart = p % 8 - 1
-    p_str = '⬤' * cFull
-    if cPart >= 0:
-        p_str += PROGRESS_INCOMPLETE[cPart]
-    p_str += ' ' * (PROGRESS_MAX_SIZE - cFull)
-    p_str = f"[{p_str}]"
+    cFull = p // 6
+    p_str = '●' * cFull
+    p_str += '○' * (16 - cFull)
+    p_str = f"{p_str}"
     return p_str
-
 
 def get_readable_message():
     with download_dict_lock:
